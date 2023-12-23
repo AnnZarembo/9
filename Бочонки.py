fadf855f52687b2d05c32ab74a2219c9add72a88
AnnZarembo <146215603+AnnZarembo@users.noreply.github.com>
2023-12-23 21:48:39 +0500

    Create Бочонки.py

---
+++ Бочонки.py
@@ -0,0 +1,45 @@
+import random
+import logging
+
+logging.basicConfig(filename='Бочонки.log', level=logging.INFO)
+
+def create_barrel_bag(N):
+    return list(range(1, N + 1))
+
+def pull_barrel(barrel_bag):
+    if len(barrel_bag) > 0:
+        pulled_barrel = random.choice(barrel_bag)
+        barrel_bag.remove(pulled_barrel)
+        logging.info(f'Вытянут бочонок: {pulled_barrel}')
+        return pulled_barrel
+    else:
+        logging.info('Бочонков в мешке больше нет.')
+        return None
+
+def s_polsovatel():
+    while True:
+        try:
+            N = int(input("Введите количество бочонков в мешке: "))
+            if N < 1:
+                print("Количество бочонков должно быть больше 0")
+                logging.error("Бочонков должно быть больше 0:")
+            else:
+                return N
+        except ValueError:
+            print("Пожалуйста, введите натуральное число.")
+            logging.error(f"Введите натуральное число: {N}")
+
+def main():
+    N = s_polsovatel()
+    logging.info(f'Запуск программы для N = {N}')
+    barrel_bag = create_barrel_bag(N)
+    random.shuffle(barrel_bag)
+    print("Вытаскиваем бочонки из мешка!")
+    while barrel_bag:
+        input("Нажмите Enter, чтобы вытянуть бочонок:")
+        pulled_barrel = pull_barrel(barrel_bag)
+        if pulled_barrel:
+            print(f"Вы вытянули бочонок с номером: {pulled_barrel}")
+
+if __name__ == "__main__":
+    main()
